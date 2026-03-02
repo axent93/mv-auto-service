80db4026e2140897f5091a5257b89b13456fe2b3
milos-aksentijevic <milos.aksentijevic@dualsoft.net>
2026-03-02 09:27:59 +0100

    test

--- src/app/(payload)/admin/importMap.js
+++ src/app/(payload)/admin/importMap.js
@@ -1,6 +1,7 @@
 import { MVAdminIcon as MVAdminIcon_9b810b0a034a4423b18630e49c564b08 } from '@/components/payload/graphics/Icon'
 import { MVAdminLogo as MVAdminLogo_0f647c81361fea2daf5fc52d3970e351 } from '@/components/payload/graphics/Logo'
 import { HeaderQuickLinks as HeaderQuickLinks_7b22a47bb4a99a251a2078b996786701 } from '@/components/payload/HeaderQuickLinks'
+import { S3ClientUploadHandler as S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24 } from '@payloadcms/storage-s3/client'
 import { AdminDashboardView as AdminDashboardView_c31221c6cc6e1b5ff09f4e4ea9e4b28a } from '@/components/payload/views/AdminDashboardView'
 import { AdminSearchView as AdminSearchView_67ce5b93664729accc4b6010c52d211e } from '@/components/payload/views/AdminSearchView'
 import { AdminVehicleDetailView as AdminVehicleDetailView_f5792cf1d490a4a8e213a397f7a4c6ee } from '@/components/payload/views/AdminVehicleDetailView'
@@ -10,6 +11,7 @@ export const importMap = {
   "@/components/payload/graphics/Icon#MVAdminIcon": MVAdminIcon_9b810b0a034a4423b18630e49c564b08,
   "@/components/payload/graphics/Logo#MVAdminLogo": MVAdminLogo_0f647c81361fea2daf5fc52d3970e351,
   "@/components/payload/HeaderQuickLinks#HeaderQuickLinks": HeaderQuickLinks_7b22a47bb4a99a251a2078b996786701,
+  "@payloadcms/storage-s3/client#S3ClientUploadHandler": S3ClientUploadHandler_f97aa6c64367fa259c5bc0567239ef24,
   "@/components/payload/views/AdminDashboardView#AdminDashboardView": AdminDashboardView_c31221c6cc6e1b5ff09f4e4ea9e4b28a,
   "@/components/payload/views/AdminSearchView#AdminSearchView": AdminSearchView_67ce5b93664729accc4b6010c52d211e,
   "@/components/payload/views/AdminVehicleDetailView#AdminVehicleDetailView": AdminVehicleDetailView_f5792cf1d490a4a8e213a397f7a4c6ee,
